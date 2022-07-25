74b8aa67f962ecbb16b65edd5ac8f54fa03911fa
Joseph E. S. Abrams <joe@pop-os.localdomain>
2022-07-25 14:12:37 -0600

    semi-working linux only

--- Projects.py
+++ Projects.py
@@ -10,17 +10,15 @@ args= parser.parse_args()
 project_name=args.project_name
 parent_dir = "./projects"
 path = os.path.join(parent_dir, project_name)
-make_path=os.path.join(path,'Makefile')
-
-
-
+make_file_name="Makefile"
+make_path=os.path.join(path,make_file_name)
 cpp_file=project_name+".cpp"
 h_file=project_name+".h"
 cpp_path=os.path.join(path,cpp_file)
 h_path=os.path.join(path,h_file)
-main_file="main.cpp"
+main_file="main-test.cpp"
 main_path=os.path.join(path,main_file)
-config_file="config.xml"
+config_file="PhysiCell_settings.xml"
 config_path=os.path.join(path,config_file)
 # backup everything
 #os.popen('make archive')
@@ -29,13 +27,11 @@ if os.path.exists(path):
     if switch_answer!= 'y' or 'yes' or 'Yes' or 'YES':
         exit()
     #switch to this project and set everything up
-    os.popen('make reset') #should probably add a line to save stuff
-    with fileinput.FileInput(make_path, inplace=True, backup='.bak') as file:
-        for line in file:
-            print(line.replace("joes_project", project_name), end='')
     os.popen(f'cp {make_path} ./')
     os.popen(f'cp {cpp_path} ./custom_modules/')
     os.popen(f'cp {h_path} ./custom_modules/')
+    os.popen(f'cp {main_path} ./')
+    os.popen(f'cp {config_path} ./config/')
 else:
     #optionally create project
     print(f'create new project named {project_name} ?')
@@ -45,7 +41,19 @@ else:
             os.mkdir(parent_dir)
         os.mkdir(path)
         # create the template files
-        #./project/templates/Makefile_template
+        os.popen(f'cp ./Makefile_template {path}')
+        print(f'{path}/Makefile_template')
+        os.popen(f'mv {path}/Makefile_template {path}/Makefile')
+        print(make_path)
+       #eventually copy all these from template directory 
+        os.popen(f'touch {h_path}')
+        os.popen(f'touch {cpp_path}')
+        os.popen(f'touch {path}/main.cpp')
+        os.popen(f'touch {path}/PhysiCell_settings.xml')
+        with fileinput.FileInput(files=make_path,inplace=True,backup='.bak') as file:
+            for line in file:
+                print(line.replace("joes_project", project_name), end='')
+        #cp from ./project/templates
         # etc
     else:
         exit()
